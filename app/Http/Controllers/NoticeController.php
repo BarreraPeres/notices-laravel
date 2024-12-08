9b334db67fe1eed0ceb5f54449ac9267c0b6a9f4
BarreraPeres <gabrielbarreraperes18@gmail.com>
2024-12-07 23:20:04 -0300

    feat: add controllers for notice, get and create

---
+++ app/Http/Controllers/NoticeController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Notice;
+use App\Models\Notification;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class NoticeController extends Controller
+{
+    public function create(Request $req)
+    {
+        $NoticeValidated = Validator::make($req->all(), [
+            "user_type" => ["required", "string"],
+            "title" => ["required", "string"],
+            "description" => ["required", "string"],
+            "author" => ["required", "string"],
+            "procedure" => ["required", "string"],
+            "brief_description" => ["required", "string"],
+            "generate_pop_up" => ["nullable", "boolean"],
+            "pop_up_expiration" => ["nullable", "date"],
+        ]);
+
+        if ($NoticeValidated->fails()) {
+            return response()->json(["message" => $NoticeValidated->errors()], 400);
+        }
+
+        $notice = Notice::create([
+            "title" => $req->title,
+            "description" => $req->description,
+            "author" => $req->author,
+            "procedure" => $req->procedure,
+            "brief_description" => $req->brief_description,
+            "generate_pop_up" => $req->generate_pop_up ?? false,
+            "pop_up_expiration" => $req->pop_up_expiration ?? null,
+        ]);
+
+        $notification = Notification::create([
+            "id_notice" => $notice->id,
+            "alias" => $notice->title,
+            "user_type" => $req->user_type,
+        ]);
+
+        return response()->json([
+            "notice" => $notice,
+            "notification" => $notification
+        ], 201);
+    }
+
+    public function get(Request $req)
+    {
+        $query = $req->query();
+        //$notices = Notice::paginate(15);
+        /** @var Notice $notice */
+        $notices = Notice::query()
+            ->whereLike("title",  $query, caseSensitive: false)
+            ->orWhereLike("description", $query, caseSensitive: false)
+            ->orWhereLike("author", $query, caseSensitive: false)
+            ->get();
+
+
+        return response()->json(
+            $notices,
+            200
+        );
+    }
+}
