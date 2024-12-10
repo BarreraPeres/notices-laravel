80547958061d2a41b303f9b1f3e6a14cb8e5546f
BarreraPeres <gabrielbarreraperes18@gmail.com>
2024-12-09 22:08:51 -0300

    feat: add routes, login, register, notice and notification

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,20 @@
 <?php
 
+use App\Http\Controllers\NoticeController;
+use App\Http\Controllers\NotificationController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+Route::post("/login", [UserController::class, "login"]);
+Route::post("/register", [UserController::class, "register"]);
+
+Route::post("/notice", [NoticeController::class, "create"]);
+Route::get("/notice", [NoticeController::class, "get"]);
+
+Route::get("/notification", [NotificationController::class, "get"])->middleware(["auth:sanctum"]);
