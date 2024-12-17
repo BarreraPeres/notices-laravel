33960ebbdd8dd82bdec4a1eb351613203afebbf3
BarreraPeres <gabrielbarreraperes18@gmail.com>
2024-12-16 22:41:25 -0300

    feat: implement accessToken,RefreshToken and logout
    - UserController.php:
    
     post - /login update in endpoit to create new access and refresh tokens
     post - /logout endpoint to revoke all tokens user
     get - /verify/logged - endpoint to verify if user is logged in
     patch - /refresh/token - endpoint to generate new token using refreshToken

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,8 +6,10 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\MakeLoginRequest;
 use App\Http\Requests\RegisterRequest;
 use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Laravel\Sanctum\PersonalAccessToken;
 
 class UserController extends Controller
 {
@@ -24,12 +26,19 @@ class UserController extends Controller
             return response()->json(["message" => "invalid credencials"], 400);
         }
 
-        $token = $user->createToken("accessToken")->plainTextToken;
+        $user->tokens()->delete();
+
+        $tenMinutes = Carbon::now()->addMinutes(10);
+        $sevenDays = Carbon::now()->addDays(7);
+        $sevenDaysForCookie = 60 * 60 * 24 * 7;
+
+        $refreshToken = $user->createToken("refreshToken", ["refresh"], $sevenDays)->plainTextToken;
+        $token = $user->createToken("accessToken", ["token"], $tenMinutes)->plainTextToken;
 
         return response()->json([
             "user" => $user,
             "token" => $token
-        ], 200);
+        ], 200)->cookie("refreshToken", $refreshToken, $sevenDaysForCookie);
     }
 
     public function register(RegisterRequest $req)
@@ -55,5 +64,54 @@ class UserController extends Controller
         return response()->json([
             "users" => $users
         ], 200);
+    }
+
+    public function logout(Request $req)
+    {
+        $req->user()->tokens()->delete();
+
+        return response()->json("ok", 200)->cookie("refreshToken", "", -1);
+    }
+
+    public function verifyLogged(Request $req)
+    {
+        $userLogged = $req->cookie("refreshToken");
+        if (!$userLogged) {
+            return response()->json(["message" => "Unauthorized"], 401);
+        }
+        return response()->json(["message" => "User logged"], 200);
+    }
+
+    public function refreshToken(Request $req)
+    {
+        $refreshToken = $req->cookie("refreshToken");
+        if (!$refreshToken) {
+            return response()->json(["message" => "Unauthorized"], 401);
+        }
+
+        $tokenModel = PersonalAccessToken::findToken($refreshToken);
+        if (!$tokenModel) {
+            return response()->json(["message" => "resource not found"], 404);
+        }
+
+        $user = $tokenModel->tokenable;
+        if (!$user) {
+            return response()->json(["message" => "resource not found"], 404);
+        }
+
+        $user->tokens()->delete();
+
+
+        $tenMinutes = Carbon::now()->addMinutes(10);
+        $sevenDays = Carbon::now()->addDays(7);
+        $sevenDaysForCookie = 60 * 60 * 24 * 7;
+
+        $newRefreshToken = $user->createToken("refreshToken", ["refresh"], $sevenDays)->plainTextToken;
+        $newToken = $user->createToken("accessToken", ["token"], $tenMinutes)->plainTextToken;
+
+        return response()->json([
+            "user" => $user,
+            "token" => $newToken
+        ], 200)->cookie("refreshToken", $newRefreshToken, $sevenDaysForCookie);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -14,11 +14,15 @@ Route::get('/user', function (Request $request) {
 //Users
 Route::post("/login", [UserController::class, "login"]);
 Route::post("/register", [UserController::class, "register"]);
-Route::get("/users", [UserController::class, "get"]);
+Route::get("/users", [UserController::class, "get"])->middleware("auth:sanctum");;
+Route::post("/logout", [UserController::class, "logout"])->middleware("auth:sanctum");
+Route::get("/verify/logged", [UserController::class, "verifyLogged"])->middleware("auth:sanctum");;
+Route::patch("/refresh/token", [UserController::class, "refreshToken"]); //->middleware("auth:sanctum");
+
 //Notices
-Route::post("/notice", [NoticeController::class, "create"]);
-Route::get("/notices", [NoticeController::class, "get"]);
+Route::post("/notice", [NoticeController::class, "create"])->middleware(["auth:sanctum"]);
+Route::get("/notices", [NoticeController::class, "get"])->middleware(["auth:sanctum"]);
 //Notifications
 Route::get("/notifications", [NotificationController::class, "get"])->middleware(["auth:sanctum"]);
 //NotificationUsers
-Route::get("notification-users", [NotificationUserController::class, "get"]);
+Route::get("notification-users", [NotificationUserController::class, "get"])->middleware(["auth:sanctum"]);
