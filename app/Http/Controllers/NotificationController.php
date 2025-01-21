b5d1abea0e0a8758dd0bf633fa3be502e65cc7e5
BarreraPeres <gabrielbarreraperes18@gmail.com>
2025-01-20 23:06:12 -0300

    feat: add get paginated notification

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -11,7 +11,7 @@ class NotificationController extends Controller
     public function get(Request $req)
     {
         $q = $req->input("q");
-        $notifications = Notification::query()->where("alias", "like", "%$q%")->get();
+        $notifications = Notification::query()->where("alias", "like", "%$q%")->orWhere("title", "like", "%$q%")->paginate(10);
 
         if (!$notifications) {
             return response()->json(["message" => "recources not found"], 404);
